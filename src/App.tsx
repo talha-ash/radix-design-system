f2e73defec557f22db283aed44a16a1cb72514a4
talha-ash <ta.xit@outlook.com>
2025-10-23 20:19:13 +0500

    add layout value from figma

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 function App() {
   return (
     <>
-      <div className="text-text-base-subtle text-heading-3 mt-4 font-inter font-bold">
-        <a href="https://vite.dev" target="_blank">
-          Hello
-        </a>
-        <button className=" m-5xl w-32 h-12 bg-bg-fill-brand-primary rounded ring-brand">Hello</button>
+      <div className="container-fixed">
+        <div className="grid-layout grid-cols-layout">
+          <div className="col-span-12 md:col-span-6 lg:col-span-4">
+            <div className="bg-blue-500 p-4 rounded">Column 1</div>
+            <div className="text-text-base-subtle text-heading-3 mt-4 font-inter font-bold">
+              <a href="https://vite.dev" target="_blank">
+                Hello
+              </a>
+              <button className="w-32 h-12 bg-bg-fill-brand-primary rounded ring-brand">
+                Hello
+              </button>
+            </div>
+          </div>
+          <div className="col-span-12 md:col-span-6 lg:col-span-4">
+            <div className="bg-red-500 p-4 rounded">Column 2</div>
+          </div>
+          <div className="col-span-12 md:col-span-6 lg:col-span-4">
+            <div className="bg-green-500 p-4 rounded">Column 3</div>
+          </div>
+        </div>
       </div>
     </>
   );
