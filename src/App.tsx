da50ae5f2fceb9bb4e5c6baccdb944a3bb30b5cb
talha-ash <ta.xit@outlook.com>
2025-10-28 10:47:52 +0500

    add layout utilities

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,20 @@ function App() {
   return (
     <>
       <div className="container-fixed">
-        <div className="grid-layout grid-cols-layout">
-          <div className="col-span-12 md:col-span-6 lg:col-span-4">
-            <div className="bg-blue-500 p-4 rounded">Column 1</div>
-            <div className="text-text-base-subtle text-heading-3 mt-4 font-inter font-bold">
-              <a href="https://vite.dev" target="_blank">
-                Hello
-              </a>
-              <button className="w-32 h-12 bg-bg-fill-brand-primary rounded ring-brand">
-                Hello
-              </button>
-            </div>
-          </div>
-          <div className="col-span-12 md:col-span-6 lg:col-span-4">
-            <div className="bg-red-500 p-4 rounded">Column 2</div>
-          </div>
-          <div className="col-span-12 md:col-span-6 lg:col-span-4">
-            <div className="bg-green-500 p-4 rounded">Column 3</div>
-          </div>
+        <div className="row">
+          <div className="grid-col-3">Columns 1</div>
+          <div className="grid-col-1">Columns 2</div>
+          <div className="grid-col-3">Columns 3</div>
+          <div>Columns 4</div>
+          <div>Columns 5</div>
+          <div>Columns 6</div>
+          <div>Columns 7</div>
+          <div>Columns 8</div>
+          <div className="grid-start-first">Columns 9</div>
+          <div>Columns 10</div>
+          <div>Columns 11</div>
+          <div>Columns 12</div>
+          <div>Columns 13</div>
         </div>
       </div>
     </>
