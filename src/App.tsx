ebde9831d7fe627247bcea5d410f0b838b332b7c
talha-ash <ta.xit@outlook.com>
2025-10-29 16:24:33 +0500

    add minimal button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
+import { Button } from "./components";
+
 function App() {
   return (
     <>
       <div className="container-fixed">
-        <div className="row">
-          <div className="grid-col-3">Columns 1</div>
-          <div className="grid-col-1">Columns 2</div>
-          <div className="grid-col-3">Columns 3</div>
-          <div>Columns 4</div>
-          <div>Columns 5</div>
-          <div>Columns 6</div>
-          <div>Columns 7</div>
-          <div>Columns 8</div>
-          <div className="grid-start-first">Columns 9</div>
-          <div>Columns 10</div>
-          <div>Columns 11</div>
-          <div>Columns 12</div>
-          <div>Columns 13</div>
+        <div className="row  place-items-center">
+          <div className="col-span-full mt-3xl">
+            <Button />
+            <h1>Hello World</h1>
+          </div>
         </div>
       </div>
     </>
---
+++ src/components/Button/button.tsx
@@ -0,0 +1,5 @@
+import "./button.css";
+
+export const Button = () => {
+  return <button className="btn btn-secondary">Hello Button</button>;
+};
