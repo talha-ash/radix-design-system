54106c92d0d10a2b96abda868a9f10698b178a6d
talha-ash <ta.xit@outlook.com>
2025-10-30 13:34:30 +0500

    add button variant in css

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,3 +1,18 @@
 export const Button = () => {
-  return <button className="btn">Hello Button</button>;
+  return (
+    <div>
+      <button className="btn btn-primary">Primary Button</button>
+      <button className="btn btn-outline-primary">Primary Button</button>
+      <button className="btn btn-tinted-primary">Primary Button</button>
+      <button className="btn btn-plain-primary">Primary Button</button>
+      <button className="btn btn-danger">Danger Button</button>
+      <button className="btn btn-outline-danger">Danger Button</button>
+      <button className="btn btn-tinted-danger">Danger Button</button>
+      <button className="btn btn-plain-danger">Danger Button</button>
+      <button className="btn btn-secondary">secondary Button</button>
+      <button className="btn btn-outline-secondary">Danger Button</button>
+      <button className="btn btn-tinted-secondary">Danger Button</button>
+      <button className="btn btn-plain-secondary">Danger Button</button>
+    </div>
+  );
 };
